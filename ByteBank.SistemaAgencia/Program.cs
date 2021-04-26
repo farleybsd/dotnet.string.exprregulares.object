5433d19ff5cb703738773201bed08bd379ec4288
Farley Rufino <farley.rufino@apinteractive.com.br>
2021-04-26 10:43:37 -0300

    IsNullOrEmpty e palavra chave string

---
+++ ByteBank.SistemaAgencia/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ByteBank.Modelos;
+using ByteBank.Modelos.Funcionarios;
+
+namespace ByteBank.SistemaAgencia
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ExtratorValorDeArgumentosURL extrator = new ExtratorValorDeArgumentosURL("");
+
+            string url = "paginas?moedaOrigem=real&moedaDestino=dolar";
+
+            int indiceInterrogacao = url.IndexOf('?');
+
+            Console.WriteLine($"indiceInterrogacao { indiceInterrogacao}" );
+
+            Console.WriteLine(url);
+
+           string argumentos  = url.Substring(indiceInterrogacao + 1); // para sair  fora do caracter ?
+
+            Console.WriteLine(argumentos);
+
+            Console.ReadLine();
+        }
+    }
+
+
+}
